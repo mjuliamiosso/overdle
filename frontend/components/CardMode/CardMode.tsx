67095af88cb93512658e57d9ac45f140abf5a7bd
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-11-04 18:41:44 -0300

    feat: home page

--- frontend/components/CardMode/CardMode.tsx
+++ frontend/components/CardMode/CardMode.tsx
@@ -1,35 +1,42 @@
 'use client'
 
 import React, { useState } from 'react';
-import { HiQuestionMarkCircle } from 'react-icons/hi';
 import styles from './CardMode.module.scss';
+import Link from 'next/link';
 
 interface CardModeProps {
+  link: string;
   background: string;
   hoverBackground: string;
+  icon: string;
   color: string;
   title: string;
   description: string;
 }
 
-const CardMode: React.FC<CardModeProps> = ({ background, hoverBackground, color, title, description }) => {
+const CardMode: React.FC<CardModeProps> = ({ link, background, hoverBackground, icon, color, title, description }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-      className={styles.card}
-      style={{
-        backgroundImage: `url(${isHovered ? hoverBackground : background})`
-      }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <HiQuestionMarkCircle style={{ color }} className={styles.icon} />
-      <div className={styles.text}>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.description}>{description}</p>
+    <Link href={link} >
+      <div
+        className={styles.card}
+        style={{
+          backgroundImage: `url(${isHovered ? hoverBackground : background})`
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <p style={{ color }} className={styles.icon}>
+          {icon}
+        </p>
+        <div className={styles.text}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.description}>{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
+
   );
 };
 

89fd147b9bd12ccdb37c9c09c056127a8333eab6
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-03-26 16:16:04 -0300

    feat: menu button component

---
+++ frontend/components/MenuButton/MenuButton.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React, { ReactNode } from "react";
+import clsx from "clsx";
+import styles from "./MenuButton.module.scss";
+
+interface MenuButtonProps {
+  icon: ReactNode;
+  link: string;
+  text?: string;
+  border?: "radius" | "noRadius";
+  show?: "none" | "block";
+  background: string;
+}
+
+const MenuButton: React.FC<MenuButtonProps> = ({
+  icon,
+  link,
+  text,
+  border = "radius",
+  show = "none",
+  background,
+}) => {
+  return (
+    <Link href={link} className={styles.menuButton}>
+      <div className={clsx(styles.icon, styles[border])} style={{ background }}>
+        {icon}
+      </div>
+      <div className={clsx(styles.text, styles[show])}>
+        <p className={styles.text}>{text}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default MenuButton;
